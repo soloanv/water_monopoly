82c52890aacda4ab5f4cf99d1addb1d071432a87
rf112 <ryguy1292@gmail.com>
2018-04-11 10:11:25 -0600

    Add files via upload

--- station.js
+++ station.js
@@ -2,6 +2,45 @@ var teamList;
 var propertyList;
 var chanceList;
 var questionList;
+var teamID;
+var questionAnswerValue; // reference database "QA" for 2=true, 1=false. initially this will equal 1 in database
+var currentLocation; // = team's currentLocation. server connection. default set to 1
+var getOutOfJailFree = false; // stored in database as true or false
+var destination;
+var gameTimer; //time remaining
+var teamCurrency;
+
+document.addEventListener("load", pageLoad(), true);
+
+function pageLoad(){
+	
+	getTeams();
+	getProperties();
+	getChance();
+	getQuestions();
+	
+	if(!sessionStorage.teamid){
+		window.location = "index.html";
+	}
+	
+	
+	var teamID = sessionStorage.teamid;
+	var questionAnswerValue = teamList[teamID-1].QA;
+	var currentLocation = teamList[teamID-1].location;
+	var getOutOfJailFree = teamList[teamID-1].jailfree;
+	var destination = teamList[teamID-1].destination;
+	var teamCurrency = Number(teamList[teamID-1].balance);
+	
+	// checking balance
+	if(teamCurrency < 0){
+		window.location = "bank-page.html";
+	}	
+	
+	checkStationType();
+	
+	//determineStation();
+	
+};
 
 function getTeams(){
 	var xmlhttp;
@@ -63,7 +102,6 @@ function getQuestions(){
 	xmlhttp.send();
 };
 
-
 function checkQA(){
 		alert("CheckQA/QA= " + teamList[sessionStorage.teamid - 1].QA);
 		if(Number(teamList[sessionStorage.teamid - 1].QA) != 2){
@@ -72,6 +110,119 @@ function checkQA(){
 			determineStation();
 		}
 };
+
+// DONE. last edit: (4/9/18)
+function checkStationType(){
+	
+	let ID = sessionStorage.teamid;
+	var currentStation = teamList[ID-1].destination;
+	var stationType = propertyList[currentStation-1].type;
+	
+	switch(Number(stationType)){ // GO type
+		case 1:
+			$('#goFreeStation').openModal();
+			break;
+		
+		
+		/*
+		case 2 : // property type	This should be done
+			var propertyOwned = // DATABASE PROPERTY OWNED INFORMATION NEEDED HERE true or false?
+			let playerAnswer;
+			
+			if(propertyOwned != -1){ // property is owned
+				let propertyOwner = // property.owner in DATABASE
+				if(propertyOwner == teamID){
+					alert("You own this property.");
+					return; // will exit this function 
+				}
+				playerAnswer = askQuestion(); // updates playerAnswer variable to true or false
+				payRent(playerAnswer);
+			}
+			if(propertyOwned == -1){ // should be -1 in database
+				playerAnswer = askQuestion();
+				if(playerAnswer == true){
+					buyProperty(currentLocation);
+				}
+			}
+			break;
+		case 3 : // chance
+			chance();
+			return;
+			break;
+			
+		case 4 : // freeParking
+			// do nothing
+			return;
+			break;
+			
+		case 5 : // Should be working (4/10/18)
+			var jailFine = 200; // how much will the fine be? flowchart showed no fine option? Is it only questions?
+			while(stationType == 5){
+				
+				var getOutOfJailFree = // get from database here
+				if (getOutOfJailFree == 1){
+					// display this button				
+					if (){
+						getOutOfJailFree = 0;
+						stationType = 4;
+						alert("You've used your card and now you're out of jail");
+					}
+				}
+// run the rest of the function inside another while(stationType == 5) (this will prevent the rest from being done if they used the getOutOfJailFree)
+				
+			// questions only happen once. if it's correct, then you get a discount on your fine. otherwise you pay full price.
+				while(stationType == 5){
+					alert("You must answer a question and pay a fine to get out of jail.");
+					var jailQuestionAnswered = askQuestion();
+					if(jailQuestionAnswered == true){
+						jailFine = 150; // fine reduced
+						alert("You got a discount on your jail fine! The amount is: " + jailFine);
+					}
+					// button to confirm payment - Vanessa GUI here
+					while(teamCurrency < jailFine){
+							alert("You do not have the funds to pay the fine. Please see CWOA administrator for additional funds");
+							// teamCurrency = new database connection to check for updated teamCurrency
+							//redirect to bank page? what do we do here?
+					}
+					if(teamCurrency >= jailFine){
+							teamCurrency = teamCurrency - jailFine; // UPDATE DATABASE HERE for teamCurrency
+							alert("You have paid the fine and are out of jail");
+					}
+					//exits inner while loop. Never have to adjust stationType.
+				}
+			}
+			break;	
+		*/
+		default:
+	}
+};
+
+function finishStation(){
+	updateQA();
+	window.location = "map-page.html";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function determineStation(){
 	
@@ -111,7 +262,6 @@ function determineStation(){
 			window.location = "map-page.html";
 	}
 };
-
 
 function updateQA(){
 	let ID = sessionStorage.teamid;
